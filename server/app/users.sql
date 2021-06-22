69b9b102d28e27a6b8b7714cc7a99fb3a6025690
yjlmobs@163.com <lewyuejian@163.com>
2021-06-22 08:10:20 +0800

    3. Flask设计User用户相关API

---
+++ server/app/users.sql
@@ -0,0 +1,7 @@
+SELECT * from user
+
+--INSERT INTO user (username, email, password_hash) VALUES('system', 'system@163.com','123');
+--INSERT INTO user (username, email, password_hash) VALUES('pophas', 'pophas@163.com','123');
+
+DELETE FROM user WHERE id=2;
+DELETE FROM user WHERE id=3;
